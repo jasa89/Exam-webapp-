c54ac69abe1d26312d473013fe40e915db01b3e8
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-05-14 17:52:44 +0300

    initial commit

---
+++ docs/research/gazetracking.md
@@ -0,0 +1,4 @@
+## selecting proper gaze tracking 
+
+### studied and tested webgazer from https://webgazer.cs.brown.edu/ 
+
---
+++ frontend/src/pages/studentspage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function studentspage() {
+  return (
+    <div>studentspage</div>
+  )
+}
