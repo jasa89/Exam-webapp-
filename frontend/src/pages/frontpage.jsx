24b9f21a2430c800f10c12c7bb6f7742c63a14b7
jasa89 <78540352+jasa89@users.noreply.github.com>
2026-05-18 17:48:26 +0300

    frontpage updated

--- frontend/src/pages/frontpage.jsx
+++ frontend/src/pages/frontpage.jsx
@@ -7,20 +7,18 @@ import "./Frontpage.css"
 
 export default function frontpage() {
   return (
-    <div>
+    <div className="frontContainer"> 
     <div className='Front-title'>
-    
+   
     <h1>Exam app</h1>
-    </div>
+   
     
-    <div className="frontContainer">
-    <img src="" alt="image of study" />
-     
-     </div>
-    
-    
-   </div>
+   <div className='image-container'> 
+  <img className='front-image' src="../images/5834.jpg" alt="image of study" />
+ </div>   
+ </div>
+  </div>
 
-  )
+)
 }
         
